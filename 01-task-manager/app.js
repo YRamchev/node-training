3f9c5dd71de4b7683d408e239d0e46a0ed001fb6
Yordan Ramchev <yordan.ramchev@MBGNM0003.local>
2022-03-27 11:22:33 +0300

    add tasks route paths

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const tasks = require("./routes/tasks");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// middleware
+app.use(express.json());
 
-app.listen(port, () => console.log(`Server listening on port ${port}...`))
+// routes
+app.use("/api/v1/tasks/", tasks);
 
+// app.get('/api/v1/tasks', (req, res) => res.send('Hello World!'))
+// app.post('/api/v1/tasks', (req, res) => res.send('Hello World!'))
+// app.get('/api/v1/tasks/:id', (req, res) => res.send('Hello World!'))
+// app.patch('/api/v1/tasks/:id', (req, res) => res.send('Hello World!'))
+// app.delete('/api/v1/tasks/:id', (req, res) => res.send('Hello World!'))
 
-console.log('Task Manager App')
+app.listen(port, () => console.log(`Server listening on port ${port}...`));
+
+console.log("Task Manager App");
---
+++ 01-task-manager/controllers/tasks.js
@@ -0,0 +1,21 @@
+const getAllTasks = (req, res) => {
+  res.send("All items");
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const updateTask = (req, res) => {
+  res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete task");
+};
+
+module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask };
