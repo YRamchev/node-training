47500553fcf476f5a4a28ea93c760755438a4cb8
Yordan Ramchev <yordan.ramchev@MBGNM0003.local>
2022-03-28 13:27:48 +0300

    init server

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 app.use(express.static("./public"));
@@ -10,12 +12,15 @@ app.use(express.json());
 // routes
 app.use("/api/v1/tasks/", tasks);
 
-// app.get('/api/v1/tasks', (req, res) => res.send('Hello World!'))
-// app.post('/api/v1/tasks', (req, res) => res.send('Hello World!'))
-// app.get('/api/v1/tasks/:id', (req, res) => res.send('Hello World!'))
-// app.patch('/api/v1/tasks/:id', (req, res) => res.send('Hello World!'))
-// app.delete('/api/v1/tasks/:id', (req, res) => res.send('Hello World!'))
+const port = 3000;
 
-app.listen(port, () => console.log(`Server listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-console.log("Task Manager App");
+start();
